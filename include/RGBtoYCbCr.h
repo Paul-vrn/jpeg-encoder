65426041b7d6b00be3fed88d5c91f0c471a80509
Marc Felix-Henry <felixhem@ensipc98.ensimag.fr>
2022-05-04 15:13:20 +0200

    Creations de Fonctions

---
+++ include/RGBtoYCbCr.h
@@ -0,0 +1,10 @@
+#ifndef _MCU_H_
+#define _MCU_H_
+
+#include <stdint.h>
+#include "bloc.h"
+#include "mcu.h"
+
+void ConversionYCbCr(uint8_t**matrice, uint32_t hauteur, uint32_t longueur)
+
+#endif /* _RGBtoYCbCr.h_ */
---
+++ tests/RGBtoYCbCr_test.c
@@ -0,0 +1,2 @@
+#include "RGBtoYCbCR.h"
+
