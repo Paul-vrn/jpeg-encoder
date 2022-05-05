21d98bd9f65f1298443cb3ace3762a219bda2cac
Paul Vernin <paul.vernin@grenoble-inp.org>
2022-05-05 13:13:35 +0200

    feat: dir tests

--- src/mcu.c
+++ src/mcu.c
@@ -9,6 +9,8 @@ struct mcu_t {
     struct bloc_t *Cb;
     struct bloc_t *Cr;
     struct mcu_t *next;
+    uint32_t largeur;
+    uint32_t hauteur;
 };
 
 void mcu_set_next(struct mcu_t *mcu, struct mcu_t *next)
@@ -26,6 +28,14 @@ void mcu_set_Cb(struct mcu_t *mcu, struct bloc_t *Cb)
 void mcu_set_Cr(struct mcu_t *mcu, struct bloc_t *Cr)
 {
     mcu->Cr = Cr;
+}
+void mcu_set_largeur(struct mcu_t *mcu, uint32_t largeur)
+{
+    mcu->largeur = largeur;
+}
+void mcu_set_hauteur(struct mcu_t *mcu, uint32_t hauteur)
+{
+    mcu->hauteur = hauteur;
 }
 struct bloc_t* mcu_get_Y(struct mcu_t *mcu)
 {
@@ -52,12 +62,14 @@ struct mcu_t* mcu_get_next(struct mcu_t *mcu)
  * @param Cr 
  * @return struct mcu_t* 
  */
-struct mcu_t *mcu_create(struct bloc_t *Y, struct bloc_t *Cb, struct bloc_t *Cr){
+struct mcu_t *mcu_create(struct bloc_t *Y, struct bloc_t *Cb, struct bloc_t *Cr, uint32_t largeur, uint32_t hauteur){
     struct mcu_t *mcu = calloc(1, sizeof(struct mcu_t));
     mcu->Y = Y;
     mcu->Cb = Cb;
     mcu->Cr = Cr;
     mcu->next = NULL;
+    mcu->largeur = largeur;
+    mcu->hauteur = hauteur;
     return mcu;
 }
 
@@ -106,26 +118,41 @@ void mcu_sous_echantillonne(struct mcu_t *mcu){
  * @param gris 
  * @return struct mcu_t* 
  */
-struct mcu_t* decoupage_mcu(uint8_t **pixels, uint32_t height, uint32_t width, bool gris){
+struct mcu_t* decoupage_mcu(uint8_t **pixels[3], uint32_t height, uint32_t width, bool gris, uint32_t L, uint32_t H, uint32_t V){
     struct mcu_t *mcu_current = NULL;
+    struct mcu_t *mcu_to_return = NULL;
     if (gris){
+        //MCU 8x8
         uint32_t start_x = 0;
         uint32_t start_y = 0;
-        while (start_x < height){
-            while (start_y < width){
-                uint32_t end_x = start_x+8<height?start_x+8:height;
-                uint32_t end_y = start_y+8<width?start_y+8:width;
-                struct bloc_t *bloc = bloc_create_from_pixels(pixels, start_x, end_x, start_y, end_y);
-                struct mcu_t *mcu = mcu_create(bloc, NULL, NULL);
-                mcu_current->next = mcu;
-                mcu_current = mcu;
-                start_y += 8;
+        while (start_y < height){
+            while (start_x < width){
+                uint32_t end_x = start_x+8<width?start_x+8:width;
+                uint32_t end_y = start_y+8<height?start_y+8:height;
+                printf("start_x: %d, end_x: %d, start_y: %d, end_y: %d\n", start_x, end_x, start_y, end_y);
+                struct bloc_t *bloc = bloc_create_from_pixels(pixels[0], start_x, end_x, start_y, end_y);
+                bloc_print(bloc);
+                struct mcu_t *new_mcu = mcu_create(bloc, NULL, NULL, 8, 8);
+                if (mcu_current == NULL){
+                    mcu_current = new_mcu;
+                    mcu_to_return = new_mcu;
+                } else {
+                    mcu_current->next = new_mcu;
+                    mcu_current = mcu_current->next;
+                }
+                start_x += 8;
             }
-            start_x += 8;
-            start_y = 0;
+            start_y += 8;
+            start_x = 0;
         }
     } else {
-
+        if (H == 4 && V == 4){
+            //MCU 8x8
+        } else if (H == 2 && V == 2){
+            //MCU 16x8
+        } else if (H == 2 && V == 0){
+            //MCU 16x16
+        }
     }
-    return mcu_current;
+    return mcu_to_return;
 }
