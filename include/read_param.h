3cb11fdac259668274aa2855611761654835447c
Axel Perrin <axel.perrin1@grenoble-inp.org>
2022-05-06 13:41:59 +0200

    feat : read parameters

---
+++ include/read_param.h
@@ -0,0 +1,19 @@
+#ifndef _READ_PARAM_H_
+#define _READ_PARAM_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+extern void read_param(int argc, char *argv[]);
+
+extern int H1 = 1;
+extern int H2 = 1;
+extern int H3 = 1;
+extern int V1 = 1;
+extern int V2 = 1;
+extern int V3 = 1;
+extern char *OUTFILE;
+extern char *INFILE;
+
+#endif /* _READ_PARAM_H_ */
---
+++ include/read_pic.h
@@ -0,0 +1,14 @@
+#ifndef _READ_PIC_H_
+#define _READ_PIC_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+uint8_t ***creat_matrix();
+
+extern uint32_t HEIGHT;
+extern uint32_t WIDTH;
+extern int TYPE;
+
+#endif /* _READ_PIC_H_ */
